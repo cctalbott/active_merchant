85d8315516e49c0eb037a0152a5981b83cab4453
Clint Talbott <cctalbott@gmail.com>
2012-02-21 18:17:53 -0600

    Adding eProcessing Network as sub class of authorize.net

---
+++ lib/active_merchant/billing/gateways/eprocessing_network.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + '/authorize_net'
+
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    class EprocessingNetworkGateway < AuthorizeNetGateway
+      self.live_url = self.test_url = 'https://www.eProcessingNetwork.Com/cgi-bin/an/order.pl'
+
+      self.homepage_url = 'http://www.eprocessingnetwork.com/'
+      self.display_name = 'eProcessingNetwork'
+
+      # Limit support to purchase() for the time being
+      # JRuby chokes here
+      # undef_method :authorize, :capture, :void, :credit
+
+      #undef_method :authorize
+      #undef_method :capture
+      #undef_method :void
+      #undef_method :credit
+
+      def test?
+        Base.gateway_mode == :test
+      end
+
+      private
+      def split(response)
+        response.split(',')
+      end
+    end
+  end
+end
